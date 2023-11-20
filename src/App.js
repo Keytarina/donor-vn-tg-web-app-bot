25ad4ab3314c3dc36336a38013f14d5237f20562
Kateryna Yakubych <kateryna.yakubych@gmail.com>
2023-11-21 01:13:52 +0200

    .

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import { useEffect } from "react";
 import { useTelegram } from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
 import { Route, Routes } from "react-router-dom";
-// import ProductList from "./components/ProductList/ProductList";
-// import Form from "./components/Form/Form";
-import Button from "./components/Button/Button";
 
 function App() {
 	const { onToggleButton, tg } = useTelegram();
