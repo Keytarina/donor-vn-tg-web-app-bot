a0eb91da2166e14f05c71ad0ccd8fed008a884f1
Kateryna Yakubych <kateryna.yakubych@gmail.com>
2023-12-27 01:10:34 +0200

    test

--- src/components/ArticlesItem/ArticlesItem.jsx
+++ src/components/ArticlesItem/ArticlesItem.jsx
@@ -1,24 +1,24 @@
-import React from "react";
-import Button from "../Button/Button";
-import "./ArticlesItem.css";
+// import React from "react";
+// import Button from "../Button/Button";
+// import "./ArticlesItem.css";
 
-const ArticlesItem = ({ product, className, onAdd }) => {
-	const onAddHandler = () => {
-		onAdd(product);
-	};
+// const ArticlesItem = ({ product, className, onAdd }) => {
+// 	const onAddHandler = () => {
+// 		onAdd(product);
+// 	};
 
-	return (
-		<div className={"product " + className}>
-			<div className={"img"} />
-			<div className={"title"}>{product.title}</div>
-			<div className={"description"}>{product.description}</div>
-			<div className={"price"}>
-				<span>
-					Стоимость: <b>{product.price}</b>
-				</span>
-			</div>
-		</div>
-	);
-};
+// 	return (
+// 		<div className={"product " + className}>
+// 			<div className={"img"} />
+// 			<div className={"title"}>{product.title}</div>
+// 			<div className={"description"}>{product.description}</div>
+// 			<div className={"price"}>
+// 				<span>
+// 					Стоимость: <b>{product.price}</b>
+// 				</span>
+// 			</div>
+// 		</div>
+// 	);
+// };
 
-export default ArticlesItem;
+// export default ArticlesItem;
